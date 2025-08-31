c332fbfa6df5b248f2bf4e24c9e9782a8057b19c
Cidy Sampaio <cidy.sampaio@gmail.com>
2025-08-31 13:01:20 -0300

    adicionado atributo alt em avatar no js

--- script.js
+++ script.js
@@ -15,8 +15,10 @@ function toggleMode() {
   if (html.classList.contains('light')) {
     // se tiver light mode, adicionar a imagem light
     img.setAttribute('src', './assets/avatar-light.png')
+    img.setAttribute('alt', 'Foto de Mayk Brito com óculos de sol')
   } else {
     // se tiver sem light mode, manter a imagem normal
     img.setAttribute('src', './assets/avatar.png')
+    img.setAttribute('alt', 'Foto de Mayk Brito de óculos')
   }
 }
